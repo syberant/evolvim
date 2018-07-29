3246713f74e7cdb6ea8ad4fff68052c465ab9c70
syberant <sybrand.s.y.b@gmail.com>
2018-07-30 00:04:19 +0100

    added brain.rs

---
+++ src/lib/brain.rs
@@ -0,0 +1,17 @@
+const OUTPUT_SIZE: usize = 11;
+
+pub struct Brain {}
+
+impl Brain {
+    pub fn input(&mut self, Vec<f64>) {
+        unimplemented!();
+    }
+
+    pub fn output(&self) -> [f64; OUTPUT_SIZE] {
+        unimplemented!();
+    }
+
+    pub fn evolve() -> Self {
+        unimplemented!();
+    }
+}
