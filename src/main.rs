cc52978d55deacd6414cb61a150cb28fcf9e5f18
syberant <sybrand.s.y.b@gmail.com>
2018-08-03 09:57:28 +0100

    added a main.rs for testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate lib_evolvim;
+use lib_evolvim::Board;
+
 fn main() {
-    println!("Hello, world!");
+    let mut board = Board::default();
+    const YEARS: usize = 1000;
+
+    for _i in 0..YEARS {
+        println!("{}", board.get_population_size());
+        for _j in 0..1000 {
+            board.update(0.001);
+        }
+    }
 }
