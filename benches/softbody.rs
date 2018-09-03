1fe1f7e58aa6be77c1dd1679a3870dd573c4a1a2
syberant <sybrand.s.y.b@gmail.com>
2018-09-03 18:38:41 +0200

    add a lot of benchmarks for `SoftBody`
    to assist research for #5

--- benches/softbody.rs
+++ benches/softbody.rs
@@ -8,17 +8,67 @@ mod benches {
     use self::lib_evolvim::*;
     use self::test::Bencher;
 
+    const TIME_STEP: f64 = 0.001;
+
     fn get_stable_870() -> Board {
         Board::load_from("assets/stable_870.bin").unwrap()
     }
 
     #[bench]
-    fn bench_softbody_collide_all(b: &mut Bencher) {
+    fn bench_softbody_collide_870(b: &mut Bencher) {
         let board = get_stable_870();
 
         b.iter(|| {
             for c_rc in &board.creatures {
                 c_rc.collide(&board.soft_bodies_in_positions);
+            }
+        });
+    }
+
+    #[bench]
+    fn bench_softbody_use_brain_870(b: &mut Bencher) {
+        let mut board = get_stable_870();
+
+        b.iter(|| {
+            let board_size = board.get_board_size();
+            let time = board.get_time();
+            let terrain = &mut board.terrain;
+            let climate = &board.climate;
+
+            for c_rc in &board.creatures {
+                c_rc.borrow_mut()
+                    .use_brain(TIME_STEP, true, time, board_size, terrain, climate);
+            }
+        })
+    }
+
+    #[bench]
+    fn bench_softbody_metabolize_870(b: &mut Bencher) {
+        let board = get_stable_870();
+
+        b.iter(|| {
+            let time = board.get_time();
+
+            for c_rc in &board.creatures {
+                c_rc.borrow_mut().metabolize(TIME_STEP, time);
+            }
+        });
+    }
+
+    #[bench]
+    fn bench_softbody_apply_motions_870(b: &mut Bencher) {
+        let mut board = get_stable_870();
+
+        b.iter(|| {
+            let board_size = board.get_board_size();
+
+            for c_rc in &board.creatures {
+                c_rc.apply_motions(
+                    TIME_STEP * 100.0,
+                    board_size,
+                    &board.terrain,
+                    &mut board.soft_bodies_in_positions,
+                );
             }
         });
     }
