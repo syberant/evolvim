1cdbf3a12fbfee7965c93ce6f2ce6bad4207ac2a
Sybrand Aarnoutse <sybrand.s.y.b@gmail.com>
2019-01-25 13:57:48 +0100

    fix tests

--- tests/brain.rs
+++ tests/brain.rs
@@ -5,8 +5,8 @@ use lib_evolvim::*;
 
 #[test]
 fn test_brain_evolve() {
-    let c_1 = HLSoftBody::from(SoftBody::Creature(Creature::new_random((100, 100), 0.0)));
-    let c_2 = HLSoftBody::from(SoftBody::Creature(Creature::new_random((100, 100), 0.0)));
+    let c_1 = HLSoftBody::from(Creature::new_random((100, 100), 0.0));
+    let c_2 = HLSoftBody::from(Creature::new_random((100, 100), 0.0));
 
     let _new_brain = Brain::evolve(&vec![c_1, c_2]);
 }
