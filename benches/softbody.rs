3ad38bb1a287622cd443bdd7bb0c7d13e92b4f1f
Sybrand Aarnoutse <sybrand.s.y.b@gmail.com>
2019-02-26 23:10:39 +0100

    deleted benches unrelated to SoftBody

--- benches/softbody.rs
+++ benches/softbody.rs
@@ -26,36 +26,6 @@ mod benches {
     }
 
     #[bench]
-    fn bench_softbody_use_brain(b: &mut Bencher) {
-        let mut board = get_test_board();
-
-        b.iter(|| {
-            let board_size = board.get_board_size();
-            let time = board.get_time();
-            let terrain = &mut board.terrain;
-            let climate = &board.climate;
-
-            for c_rc in &board.creatures {
-                c_rc.borrow_mut()
-                    .use_brain(TIME_STEP, time, board_size, terrain, climate);
-            }
-        })
-    }
-
-    #[bench]
-    fn bench_softbody_update_brain(b: &mut Bencher) {
-        let mut board = get_test_board();
-
-        b.iter(|| {
-            let terrain = &mut board.terrain;
-
-            for c_rc in &board.creatures {
-                c_rc.borrow_mut().update_brain(terrain);
-            }
-        })
-    }
-
-    #[bench]
     fn bench_softbody_metabolize(b: &mut Bencher) {
         let board = get_test_board();
 
