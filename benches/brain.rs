94ead1cb99214a579982f64a22046e30b04e7088
Sybrand Aarnoutse <sybrand.s.y.b@gmail.com>
2019-02-26 23:32:23 +0100

    last fixes for benchmarks new testing Board

--- benches/brain.rs
+++ benches/brain.rs
@@ -8,9 +8,13 @@ mod benches {
     use self::lib_evolvim::*;
     use self::test::Bencher;
 
-    const TEST_INPUT: BrainInput = [1., 2., 3., 4., 5., 6., 7., 8., 9.];
+    // const TEST_INPUT: BrainInput = [1., 2., 3., 4., 5., 6., 7., 8., 9.];
     const TEST_BOARD_SIZE: BoardSize = (100, 100);
     const TEST_TIME: f64 = 0.0;
+
+    fn get_test_board() -> Board {
+        Board::load_from("assets/test.bin").unwrap()
+    }
 
     #[bench]
     fn bench_brain_new_random(b: &mut Bencher) {
@@ -18,46 +22,56 @@ mod benches {
     }
 
     #[bench]
-    fn bench_brain_run(b: &mut Bencher) {
-        let mut brain = Brain::new_random();
+    fn bench_brain_run_with(b: &mut Bencher) {
+        let mut board = get_test_board();
+        let creature: &mut SoftBody = &mut board.creatures[0].borrow_mut();
+        let brain = &mut creature.brain;
+        let env = Environment::new(&board.terrain, &creature.base);
 
         b.iter(|| {
-            brain.run(TEST_INPUT);
+            brain.run_with(&env);
         });
     }
 
     #[bench]
     fn bench_brain_load_input(b: &mut Bencher) {
-        let mut brain = Brain::new_random();
+        let mut board = get_test_board();
+        let creature: &mut SoftBody = &mut board.creatures[0].borrow_mut();
+        let brain = &mut creature.brain;
+        let env = Environment::new(&board.terrain, &creature.base);
 
-        b.iter(|| brain.load_input(TEST_INPUT));
+        b.iter(|| brain.load_input(&env));
     }
 
     #[bench]
     fn bench_brain_feed_forward(b: &mut Bencher) {
-        let mut brain = Brain::new_random();
-        brain.load_input(TEST_INPUT);
+        let mut board = get_test_board();
+        let creature: &mut SoftBody = &mut board.creatures[0].borrow_mut();
+        let brain = &mut creature.brain;
+        let env = Environment::new(&board.terrain, &creature.base);
 
-        b.iter(|| brain.feed_forward());
+        brain.load_input(&env);
+
+        b.iter(|| brain.run());
     }
 
     #[bench]
     fn bench_brain_evolve_1_parent(b: &mut Bencher) {
-        let parents = vec![HLSoftBody::from(SoftBody::new_random_creature(
+        let parents = vec![HLSoftBody::from(SoftBody::new_random(
             TEST_BOARD_SIZE,
             TEST_TIME,
         ))];
 
-        b.iter(|| Brain::evolve(&parents));
+        b.iter(|| Brain::recombination_infinite_parents(&parents));
     }
 
     #[bench]
     fn bench_brain_evolve_2_parents(b: &mut Bencher) {
         let parents = vec![
-            HLSoftBody::from(SoftBody::new_random_creature(TEST_BOARD_SIZE, TEST_TIME)),
-            HLSoftBody::from(SoftBody::new_random_creature(TEST_BOARD_SIZE, TEST_TIME)),
+            HLSoftBody::from(SoftBody::new_random(TEST_BOARD_SIZE, TEST_TIME)),
+            HLSoftBody::from(SoftBody::new_random(TEST_BOARD_SIZE, TEST_TIME)),
         ];
 
-        b.iter(|| Brain::evolve(&parents));
+        b.iter(|| Brain::recombination_infinite_parents(&parents));
     }
 }
