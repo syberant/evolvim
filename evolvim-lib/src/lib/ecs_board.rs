c1022bcea60529ba5a5c6c6c241421080c16e923
Sybrand Aarnoutse <sybrand.s.y.b@gmail.com>
2019-06-16 12:14:36 +0200

    add dispatcher

--- evolvim-lib/src/lib/ecs_board.rs
+++ evolvim-lib/src/lib/ecs_board.rs
@@ -3,11 +3,12 @@ use crate::brain::Brain;
 use crate::constants;
 use crate::softbody::Creature;
 
-pub struct ECSBoard {
+pub struct ECSBoard<'a, 'b> {
     world: specs::World,
+    dispatcher: specs::Dispatcher<'a, 'b>,
 }
 
-impl ECSBoard {
+impl<'a, 'b> ECSBoard<'a, 'b> {
     pub fn init(board_size: BoardSize, noise_step_size: f64) -> Self {
         let mut world = specs::World::new();
 
@@ -31,30 +32,36 @@ impl ECSBoard {
 
         world.add_resource(board_size);
 
-        // Return the world
-        ECSBoard { world }
+        // Make the world
+        let mut board = ECSBoard {
+            world,
+            dispatcher: specs::DispatcherBuilder::new().build(),
+        };
+
+        // Build the dispatcher
+        board.custom_dispatcher(specs::DispatcherBuilder::new());
+
+        return board;
     }
 
     pub fn run(&mut self) {
-        use specs::RunNow;
-        use crate::systems::*;
-
-        let mut res_up = UpdateResources;
-        let mut creat_up = UpdateCreatures;
-        let mut rm_dead_creat = RemoveDeadCreatures;
-        let mut creat_rep = CreaturesReproduce;
-        let mut refill_creat = RefillCreatures;
-        let mut physics = PhysicsStep;
-
-        res_up.run_now(&mut self.world.res);
-        creat_up.run_now(&mut self.world.res);
-        rm_dead_creat.run_now(&mut self.world.res);
-        creat_rep.run_now(&mut self.world.res);
-        refill_creat.run_now(&mut self.world.res);
-        physics.run_now(&mut self.world.res);
+        self.dispatcher.dispatch(&mut self.world.res);
 
         // Synchronize deletions and insertions
         self.world.maintain();
+    }
+
+    pub fn custom_dispatcher(&mut self, builder: specs::DispatcherBuilder<'a, 'b>) {
+        use crate::systems::*;
+
+        self.dispatcher = builder
+            .with(UpdateResources, "update_resources", &[])
+            .with(UpdateCreatures, "update_creatures", &["update_resources"])
+            .with(RemoveDeadCreatures, "remove_dead_creatures", &["update_creatures"])
+            .with(CreaturesReproduce, "creatures_reproduce", &["remove_dead_creatures"])
+            .with(RefillCreatures, "refill_creatures", &["creatures_reproduce"])
+            .with(PhysicsStep, "physics_step", &["refill_creatures"])
+            .build();
     }
 
     pub fn get_time(&self) -> f64 {
